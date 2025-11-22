4b335e2ed70c310c86bed1674131ac1d6b58660e
yassineoo <jy_attou@esi.dz>
2025-11-22 17:34:45 +0100

    first commit

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Logo: React.FC<{ className?: string }> = ({ className = "w-24 h-24" }) => {
   return (
     <div className={`flex flex-col items-center ${className}`}>
-      <img src={"./public/logo.png"} alt="Logo" className="object-contain h-12 w-28" />
+      <img src={"./logo.png"} alt="Logo" className="object-contain h-12 w-28" />
       <h1 className="text-2xl font-bold tracking-widest text-white mt-2 uppercase">Penista</h1>
     </div>
   );
